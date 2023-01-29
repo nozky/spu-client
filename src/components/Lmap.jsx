7c227f65971844dc17d5dc220a0c564b23cc7fde
nozky <norvillie.villaruel@edu.sait.ca>
2023-01-29 01:08:17 -0700

    update files

--- src/components/Lmap.jsx
+++ src/components/Lmap.jsx
@@ -1,14 +1,15 @@
-import React,{ useState } from 'react'
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import './lmap.css'
-import solarActive from '../icons/solarActive.png'
-import solarInactive from '../icons/solarInactive.png'
-import * as L from 'leaflet'
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./lmap.css";
+import solarActive from "../icons/solarActive.png";
+import solarInactive from "../icons/solarInactive.png";
+import * as L from "leaflet";
 
 const activeIcon = new L.Icon({
   iconUrl: solarActive,
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
@@ -17,61 +18,77 @@ const activeIcon = new L.Icon({
 
 const inactiveIcon = new L.Icon({
   iconUrl: solarInactive,
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
 
-
-const Lmap = ({spu}) => {
-  
+const Lmap = ({ spu }) => {
   // center map to philippines
-  const [mapCenter, setMapCenter] = useState([12.8797,121.7740])
+  const [mapCenter, setMapCenter] = useState([12.8797, 121.774]);
 
-
-  if( !Array.isArray( spu ) ){
-    return(
-      <div style={{textAlign: 'center',padding: '3em'}}>
+  console.log(spu);
+  if (!Array.isArray(spu)) {
+    return (
+      <div style={{ textAlign: "center", padding: "3em" }}>
         <h2> Something is went wrong!</h2>
       </div>
-    )
+    );
   }
 
   return (
-    <MapContainer 
-      style={{width: '100%', height: '100%'}}
-      zoom={6} 
-      center={mapCenter} 
-      scrollWheelZoom={true} >
-       <TileLayer
-         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-       />
+    <MapContainer
+      style={{ width: "100%", height: "100%" }}
+      zoom={6}
+      center={mapCenter}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-      {spu.map((user)=>{
+      {spu.map((user) => {
+        let position = [user.position.lat, user.position.lng];
 
-        let position = [user.position.lat, user.position.lng]
+        if (user.deleted === true) return null;
 
-        if( user.deleted === true) return null 
-
-        return(
-          <Marker key={user._id} position={position} icon={ user.active? activeIcon : inactiveIcon  } >
+        return (
+          <Marker
+            key={user._id}
+            position={position}
+            icon={user.active ? activeIcon : inactiveIcon}
+          >
             <Popup>
-              <span style={{fontStyle:'italic', fontWeight: 'bold'}}>{ user.details.name }</span>  <br /> 
-              <span style={{fontStyle:'italic', fontWeight: 'bold'}}>System power:</span> {  user.details.power < 1? user.details.power.toFixed(3) : user.details.power.toFixed(0) } kw <br /> 
-              { user.details.info } <br />
-              <span style={{fontStyle:'italic', fontWeight: 'bold'}}>Contact:</span> <a href={`mailto: ${user.email}`}>{user.email}</a> <br />
-              <span style={{fontStyle:'italic', fontWeight: 'bold'}}>Active:</span> {user.active? "Yes" : "No" }
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                {user.details.name}
+              </span>{" "}
+              <br />
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                System power:
+              </span>{" "}
+              {user.details.power < 1
+                ? user.details.power.toFixed(3)
+                : user.details.power.toFixed(0)}{" "}
+              kw <br />
+              {user.details.info} <br />
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                Contact:
+              </span>{" "}
+              <a href={`mailto: ${user.email}`}>{user.email}</a> <br />
+              <span style={{ fontStyle: "italic", fontWeight: "bold" }}>
+                Active:
+              </span>{" "}
+              {user.active ? "Yes" : "No"}
             </Popup>
-        </Marker>
-        )
+          </Marker>
+        );
       })}
-
     </MapContainer>
-  )
-}
+  );
+};
 
-
-export default Lmap
+export default Lmap;
