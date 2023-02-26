d4acefc1b706d86769e2bc81ccd1e4f3bb2bf4a6
nozky <norvillie.villaruel@edu.sait.ca>
2023-02-26 00:44:11 -0700

    update files

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Version: 1.0
 
 ---
 
-Solar Power Philippines is a group of people that believe solar power technology is the next big thing.
+Solar Power Philippines is a community of individuals who strongly believe that solar power technology has immense potential and will play a significant role in the future. They are committed to promoting the use of solar power as a sustainable and eco-friendly alternative to traditional power sources.
 
-This app show's how many users already using solar power as thier primary or backup system incase of power grid failure. 
+This mobile application displays the number of users who have already made the switch to solar power as their primary or backup system, especially in cases of power grid failure. It serves as a platform for users to connect, share their experiences, and learn more about solar power technology.
 
 ---
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,45 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './about.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./about.css";
 
 const About = () => {
   return (
-  <section className='about'>
+    <section className="about">
       <div className="about-head">
         <h2>SPP</h2>
         <p>Solar Power Philippines</p>
       </div>
 
-      <div className='about-text space'>
-        <p>Solar Power Philippine is made so we can have a visual representation of how many people already using solar power.</p>
-        <p>This is not limited to any group, it is for everyone consumption.</p>
+      <div className="about-text space">
+        <p>
+          Solar Power Philippines is a platform created to visually demonstrate
+          the number of people who are using solar power. This helps to promote
+          the use of solar power and increase awareness of its benefits as a
+          renewable energy source.
+        </p>
+        <p>
+          This platform is accessible to everyone and not limited to any
+          specific group. It is open for public consumption and available for
+          anyone interested in solar power.
+        </p>
       </div>
 
       <div>
         <p>Maintained by: A member of Solar Power Philippines</p>
       </div>
 
-      <div className='btn'>
+      <div className="btn">
         <p>Please email of any concern</p>
-        <a href="mailto:spup2021@gmail.com?subject=Mail from Our Site">Send mail</a>
+        <a href="mailto:spup2021@gmail.com?subject=Mail from Our Site">
+          Send mail
+        </a>
       </div>
 
-      <div className='space'>
-        <p>Security notice: Please do not put your actual email password, instead create a new one.</p>
-        <p>All Password is being hash or encrpted, even me as a developer will not be able to access your information.</p>
+      <div className="space">
+        <p>
+          For security reasons, please avoid using your actual email password
+          and create a new one instead.
+        </p>
+        <p>
+          Rest assured that all passwords are being encrypted or hashed on this
+          platform. As the developer, I will not be able to access your personal
+          information.
+        </p>
       </div>
 
-      
-      <div className='btn'>
-        <Link to='/'>Map</Link>
+      <div className="btn">
+        <Link to="/">Map</Link>
       </div>
-    
 
       <footer>
         <p>Solar Power Philippines license: MIT 2021</p>
       </footer>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
